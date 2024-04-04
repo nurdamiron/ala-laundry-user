d0bd91b665a1a046db713eb50b16a0d7dc82d548
Nurdaulet Akhmatov <70795559+nurdamiron@users.noreply.github.com>
2024-04-04 09:14:49 +0500

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'assets/css/App.css';
+import { HashRouter, Route, Switch, Redirect } from 'react-router-dom';
+import AuthLayout from 'layouts/auth';
+import AdminLayout from 'layouts/admin';
+import RoomLayout from "layouts/room";
+import MachineLayout from "layouts/machine";
+
+import RtlLayout from 'layouts/rtl';
+import { ChakraProvider } from '@chakra-ui/react';
+import theme from 'theme/theme';
+import { ThemeEditorProvider } from '@hypertheme-editor/chakra-ui';
+
+ReactDOM.render(
+  <ChakraProvider theme={theme}>
+    <React.StrictMode>
+      <ThemeEditorProvider>
+        <HashRouter>
+          <Switch>
+            <Route path={`/auth`} component={AuthLayout} />
+            <Route path={`/admin`} component={AdminLayout} />
+            <Route path={`/rooms`} component={RoomLayout} />
+            <Route path={`/machine`} component={MachineLayout} />
+            <Redirect from="/" to="/admin/default" />
+          </Switch>
+        </HashRouter>
+      </ThemeEditorProvider>
+    </React.StrictMode>
+  </ChakraProvider>,
+  document.getElementById("root")
+);
